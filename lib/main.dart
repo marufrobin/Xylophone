1851f18fcb2df211ad1e631a8ea72c2e1ab9dea8
Maruf Robin <maruf.robin@northsouth.edu>
2022-09-14 13:52:11 +0600

    adding pakcage practice.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(Xylophone());
 }
 
 class Xylophone extends StatelessWidget {
+  List words = [];
+  genratreswords() {
+    generateWordPairs().take(100).forEach(
+      (element) {
+        words.add(element.toString());
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    genratreswords();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Xylophone"),
           centerTitle: true,
         ),
+        body: Text(words.length.toString()),
       ),
     );
   }
