24ff3a18fac4fd91af90e91c1ea647370911d8e3
Maruf Robin <maruf.robin@northsouth.edu>
2022-09-15 21:37:14 +0600

    Modify the UI and readme page(Project DONE).

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # xylophone
 
-A new Flutter project Called "Xylophone". Here, I will build application that make sound with press of button.
-![Xylophone](https://user-images.githubusercontent.com/47666475/190356903-fbadf19f-d9ff-40af-97a3-1cb8c885a38d.png)
+A new Flutter project Called "Xylophone". Here, this Application primary function is to play various sound on button click.
+Here is the example in Screenshot.
+
+==
+![Xylophone](https://user-images.githubusercontent.com/47666475/190445595-3c721f92-0e90-42ab-ab14-3328ea8c049f.gif)
+===
+![Xylophone](https://user-images.githubusercontent.com/47666475/190444881-738e8519-6a66-4e24-a063-03a5a2c78428.png)
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,21 @@ class Xylophone extends StatelessWidget {
   //     },
   //   );
   // }
+
   Widget noteContainer({required int noteNumber, required Color clr}) {
-    return Container(
-      color: clr,
-      child: TextButton(
-        onPressed: () async {
-          final player = AudioPlayer();
-          // await player.setSource(AssetSource('/assets/note1.wav'));
-          await player.play(AssetSource('note$noteNumber.wav'));
-        },
-        child: Text(
-          'Note $noteNumber.',
-          style: TextStyle(color: Colors.black),
+    return Expanded(
+      child: Container(
+        color: clr,
+        child: TextButton(
+          onPressed: () async {
+            final player = AudioPlayer();
+            // await player.setSource(AssetSource('/assets/note1.wav'));
+            await player.play(AssetSource('note$noteNumber.wav'));
+          },
+          child: Text(
+            'Note $noteNumber',
+            style: TextStyle(color: Colors.black, fontSize: 25),
+          ),
         ),
       ),
     );
@@ -37,20 +40,24 @@ class Xylophone extends StatelessWidget {
     // genratreswords();
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Xylophone"),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            noteContainer(clr: Colors.red, noteNumber: 1),
-            noteContainer(clr: Colors.orange, noteNumber: 2),
-            noteContainer(clr: Colors.yellow, noteNumber: 3),
-            noteContainer(clr: Colors.green, noteNumber: 4),
-            noteContainer(clr: Colors.teal, noteNumber: 5),
-            noteContainer(clr: Colors.blue, noteNumber: 6),
-            noteContainer(clr: Colors.purple, noteNumber: 7)
-          ],
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   title: Text("Xylophone"),
+        //   centerTitle: true,
+        // ),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              noteContainer(clr: Colors.red, noteNumber: 1),
+              noteContainer(clr: Colors.orange, noteNumber: 2),
+              noteContainer(clr: Colors.yellow, noteNumber: 3),
+              noteContainer(clr: Colors.green, noteNumber: 4),
+              noteContainer(clr: Colors.teal, noteNumber: 5),
+              noteContainer(clr: Colors.blue, noteNumber: 6),
+              noteContainer(clr: Colors.purple, noteNumber: 7)
+            ],
+          ),
         ),
       ),
     );
